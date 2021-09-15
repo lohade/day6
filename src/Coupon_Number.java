15f3c8ab9a8bdba823b744d21055166a093b4e71
lohade <88436159+lohade@users.noreply.github.com>
2021-09-15 12:38:51 +0530

    Update Coupon_Number.java

--- src/Coupon_Number.java
+++ src/Coupon_Number.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
-
-
-
-
-public class Coupon_Number {
-    public static double random(){
-        return (Math.random());
+class Coupon_Number {
+    public static int getCoupon(int n){
+        int ran= (int)(Math.random()*n);
+        return ran;
     }
 
-
-
     public static void main(String[] args){
-        System.out.println("enter number to finding distinct number");
+        System.out.println("enter number to finding distinct number:");
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        boolean[] isCollected=new boolean[n];
         int count=0;
+        int distinct=0;
 
-
+        while(distinct<n){
+            //System.out.println("Random" +getCoupon(n));
+            int value=getCoupon(n);
+            count++;
+            if(isCollected[value]==false){
+                distinct++;
+                isCollected[value]=true;
+            }
+        }
+        System.out.println("for find distinct value random function called:"+count +"times");
+        System.out.println("");
     }
 }
